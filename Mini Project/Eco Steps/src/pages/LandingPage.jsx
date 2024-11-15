89f9b65959d1425326cf77953c5c8b220ac50c85
KhrAini <ayni110603@gmail.com>
2024-11-15 22:46:22 +0700

    update landing page

--- Mini Project/Eco Steps/src/pages/LandingPage.jsx
+++ Mini Project/Eco Steps/src/pages/LandingPage.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import { useNavigate } from 'react-router-dom';
 import leg from '../assets/leg.png';
 
 const LandingPage = () => {
-  const navigate = useNavigate(); // Hook untuk navigasi programatik
+  const navigate = useNavigate();
 
   const handleGetStarted = () => {
-    navigate('/login'); // Arahkan ke halaman login
+    navigate('/login');
   };
 
   return (
     <div className="min-h-screen bg-[#f8f8f8] overflow-y-scroll m-0 p-0">
-      {/* Section utama dengan gambar */}
       <div className="flex min-h-screen bg-[#42632d] m-0 p-0">
-        {/* Konten teks di kiri */}
         <div className="flex-1 flex items-center px-6 md:px-12">
           <div className="text-[#f8f8f8]">
             <h1 className="text-4xl md:text-8xl font-extrabold mb-4 text-left">
@@ -25,7 +23,6 @@ const LandingPage = () => {
           </div>
         </div>
 
-        {/* Gambar di kanan */}
         <div className="flex-1 h-auto">
           <img 
             src={leg} 
@@ -35,7 +32,6 @@ const LandingPage = () => {
         </div>
       </div>
 
-      {/* Section tambahan di bawah gambar */}
       <div className="bg-[#f8f8f8] py-10">
         <div className="flex flex-col items-center text-white text-center">
           <p className="text-3xl text-[#00703c] font-semibold">
@@ -44,7 +40,7 @@ const LandingPage = () => {
         </div>
         <div className="flex justify-center items-center mt-4">
           <button 
-            onClick={handleGetStarted} // Menambahkan event handler untuk tombol
+            onClick={handleGetStarted}
             className="bg-[#00703c] text-[#f8f8f8] font-semibold py-3 px-8 rounded-full hover:bg-black transition duration-300"
           >
             Get started
